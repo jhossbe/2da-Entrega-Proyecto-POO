b8a46e281c529788cb2e4fcec355fd5245251c1a
aedelarosa <aedelarosa.24@est.ucab.edu.ve>
2025-06-26 18:10:02 -0400

    Despliegue de pantalla completa automáticamente

--- triviaucab1/src/main/java/org/example/triviaucab1/HelloApplication.java
+++ triviaucab1/src/main/java/org/example/triviaucab1/HelloApplication.java
@@ -11,9 +11,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Tablero");
         stage.setScene(scene);
+        // This line maximizes the window
+        stage.setMaximized(true);
         stage.show();
     }
 
