8835fcd99b75ff5b4f52c0d7098d57090a6c59c8
jhossbe <jmbello.24@est.ucab.edu.ve>
2025-06-26 17:47:37 -0400

    Add files via upload

---
+++ triviaucab1/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.triviaucab1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens org.example.triviaucab1 to javafx.fxml;
+    exports org.example.triviaucab1;
+    exports org.example.triviaucab1.controller;
+    opens org.example.triviaucab1.controller to javafx.fxml;
+    exports org.example.triviaucab1.module;
+    opens org.example.triviaucab1.module to javafx.fxml;
+}
